8cac3932fb3464dbb29607279dd5ae035049ea42
Brian Takita <btakita@truecar.com>
2012-02-08 16:24:38 -0800

    v0.13.0

--- generators/jelly/templates/javascripts/jelly.js
+++ generators/jelly/templates/javascripts/jelly.js
@@ -1,7 +1,7 @@
 /**
  *  Jelly. a sweet unobtrusive javascript framework for Rails
  *
- *  version 0.12.0
+ *  version 0.13.0
  *
  * Copyright (c) 2009 Pivotal Labs
  * Licensed under the MIT license.
